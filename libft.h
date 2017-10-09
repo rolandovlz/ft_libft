08e261d14850a7fc7791d79495628adc2c2767f4
rolandovlz <rolandovlz@gmail.com>
2017-10-09 00:31:21 -0700

    Added missing prototypes.

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rvelez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/23 12:23:23 by rvelez            #+#    #+#             */
-/*   Updated: 2017/10/08 19:23:31 by rvelez           ###   ########.fr       */
+/*   Updated: 2017/10/09 00:23:11 by rvelez         ###   ########.fr         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,12 +49,16 @@ int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
 int				ft_isascii(int c);
+int				ft_isprint(int c);
 int				ft_toupper(int c);
 int				ft_tolower(int c);
 void			*ft_memalloc(size_t size);
 void			ft_memdel(void **ap);
 char			*ft_strnew(size_t size);
 void			ft_strdel(char **as);
+void			ft_strclr(char *s);
+void			ft_striter(char *s, void (*f)(char *));
+void			ft_striteri(char *s, void (*f)(unsigned int, char *));
 char			*ft_strmap(char const *s, char (*f)(char));
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int				ft_strequ(char const *s1, char const *s2);
@@ -63,6 +67,7 @@ char			*ft_strsub(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(const char *s);
 char			**ft_strsplit(char const *s, char c);
+char			*ft_itoa(int n);
 void			ft_putchar(char c);
 void			ft_putstr(char const *s);
 void			ft_putendl(char const *s);
