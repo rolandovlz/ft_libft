211d3b4f60585f607d2685f1bbf63ab03c53b498
rolandovlz <rolandovlz@gmail.com>
2017-10-09 00:44:26 -0700

    strcmp fix

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: rvelez <rvelez@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/28 14:21:45 by rvelez            #+#    #+#             */
-/*   Updated: 2017/09/28 16:27:17 by rvelez           ###   ########.fr       */
+/*   Updated: 2017/10/09 00:41:30 by rvelez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,22 @@
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	size_t	i;
+	size_t	s1_len;
+	size_t	s2_len;
 
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if (s1_len == 0 && s2_len == 0)
+		return (0);
 	i = 0;
-	if (ft_strlen(s1) == 0)
-		return (0 - (unsigned char)s2[i]);
-	while (s1[i])
+	if (s1_len > 0)
 	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
+		while (i < s1_len)
+		{
+			if (s1[i] != s2[i])
+				return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+			i++;
+		}
 	}
-	return (0);
+	return (-(unsigned char)s2[i]);
 }
